5f8fe68304d29def5cfaf4c6f155d568c6a2dc95
Ivan Tan <ivanxtan@gmail.com>
2019-10-13 03:03:56 -0700

    Added semantic-ui components

--- src/components/container/HabitForm/HabitForm.js
+++ src/components/container/HabitForm/HabitForm.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import { Redirect } from "react-router-dom";
 
+import { Form, Button, TextArea, Label } from "semantic-ui-react";
+
 function HabitForm(props) {
     let [name, setName] = useState("");
     let [description, setDescription] = useState("");
@@ -20,17 +22,19 @@ function HabitForm(props) {
         );
     } else {
         return (
-            <form onSubmit={onSubmit}>
-                <label>
-                    Name
-                    <input name="name" type="text" value={name} onChange={(e) => { setName(e.target.value) }}></input>
-                </label>
-                <label>
-                    Description
-                    <input name="description" type="text" value={description} onChange={(e) => { setDescription(e.target.value) }}></input>
-                </label>
-                <button type="submit" disabled={disableSubmit} >Submit</button>
-            </form>
+            <Form onSubmit={onSubmit}>
+              <Form.Field inline>
+                <label>I want to...</label>
+                <input required name="name" type="text" value={name} onChange={(e) => { setName(e.target.value) }} placeholder="learn to play piano"/>
+                {!name ? (<Label basic color='red' pointing='left'>You must enter a habit!</Label>) : null}
+              </Form.Field>
+              <Form.Field>
+                <label>Description</label>
+                <TextArea required name="description" type="text" value={description} onChange={(e) => { setDescription(e.target.value) }} placeholder="I want to learn a new skill: playing the piano!" />
+                {!description ? (<Label basic color='red' pointing>You must enter a description!</Label>) : null}
+              </Form.Field>
+              <Button type='submit' disabled={disableSubmit}>Submit</Button>
+            </Form>
         );
     }
 }
