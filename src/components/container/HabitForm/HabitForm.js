c54728e39a5bbd9221eed3e4e7de34988fc4828a
Ivan Tan <ivanxtan@gmail.com>
2019-10-13 04:43:16 -0700

    Removed required labels

--- src/components/container/HabitForm/HabitForm.js
+++ src/components/container/HabitForm/HabitForm.js
@@ -25,14 +25,13 @@ function HabitForm(props) {
     } else {
         return (
             <Form onSubmit={onSubmit}>
-              <Form.Field inline>
+              <Form.Field>
                 <Input label={
                   <Label><Icon name="clipboard list" />I want to...</Label>
                 } required name="name" type="text" value={name} onChange={(e) => { setName(e.target.value) }} placeholder="learn to play piano"/>
-                {!name ? (<Label basic color="red" pointing="left">You must enter a habit!</Label>) : null}
               </Form.Field>
               <Form.Field>
-                <label>Description <Icon name="content" />{!description ? (<Label basic color="red">You must enter a description!</Label>) : null}</label>
+                <label>Description</label>
                 <TextArea className={styles.description} required name="description" type="text" value={description} onChange={(e) => { setDescription(e.target.value) }} placeholder="I want to learn a new skill: playing the piano!" />
               </Form.Field>
               <Button type='submit' disabled={disableSubmit}>Submit</Button>
