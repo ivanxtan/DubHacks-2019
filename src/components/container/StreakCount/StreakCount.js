6b439a9dd9ac9fd9e71d5afa198065f26c670b3f
yehgen <yehgen@uw.edu>
2019-10-13 05:37:26 -0700

    Aligned to center

--- src/components/container/StreakCount/StreakCount.js
+++ src/components/container/StreakCount/StreakCount.js
@@ -8,21 +8,20 @@ function StreakCount() {
     let habitContext = useContext(HabitContext);
     if(habitContext.habits.length === 0) {
       return null;
-    }
-    else {
+    } else {
       let maxnum = 0;
       let maxhab = "";
 
-      for(let i = 0; i < habitContext.habits.length; i++) {
+      for (let i = 0; i < habitContext.habits.length; i++) {
         let hab1 = habitContext.habits[i];
-        if(hab1.currentStreak >= maxnum) {
+        if (hab1.currentStreak >= maxnum) {
           maxnum = hab1.currentStreak;
           maxhab = hab1.info.name;
         }
       }
       return (
         <>
-          <Header as="h1"><Icon name='fire' />Your longest current streak is {maxnum} day{maxnum !== 1 ? "s" : null} to {maxhab}</Header>
+          <Header as="h1" textAlign="center"><Icon name='fire' />Your longest current streak is {maxnum} day{maxnum !== 1 ? "s" : null} to {maxhab}!</Header>
         </>
       );
   }
