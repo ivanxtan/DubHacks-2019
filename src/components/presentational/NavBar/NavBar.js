349f33cac5266cafeb50590bf81dd31e6eec4c99
Ivan Tan <ivanxtan@gmail.com>
2019-10-13 00:53:28 -0700

    Created NavBar

--- src/components/presentational/NavBar/NavBar.js
+++ src/components/presentational/NavBar/NavBar.js
@@ -1,9 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+import styles from "./NavBar.module.css";
+
 function NavBar() {
     return (
-        <nav>
+        <nav className={styles.container}>
             <ul>
                 <li><Link to={`/`}>Home</Link></li>
                 <li><Link to={`/create`}>Create</Link></li>
