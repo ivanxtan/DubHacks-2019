85f03298c5a7125c06ff8135c453d96c570620fe
Ivan Tan <ivanxtan@gmail.com>
2019-10-13 03:21:55 -0700

    Added icons

--- src/components/container/HabitForm/HabitForm.js
+++ src/components/container/HabitForm/HabitForm.js
@@ -1,7 +1,9 @@
 import React, { useState } from "react";
 import { Redirect } from "react-router-dom";
 
-import { Form, Button, TextArea, Label } from "semantic-ui-react";
+import { Form, Button, TextArea, Label, Icon, Input } from "semantic-ui-react";
+
+import styles from "./HabitForm.module.css";
 
 function HabitForm(props) {
     let [name, setName] = useState("");
@@ -24,14 +26,13 @@ function HabitForm(props) {
         return (
             <Form onSubmit={onSubmit}>
               <Form.Field inline>
-                <label>I want to...</label>
-                <input required name="name" type="text" value={name} onChange={(e) => { setName(e.target.value) }} placeholder="learn to play piano"/>
-                {!name ? (<Label basic color='red' pointing='left'>You must enter a habit!</Label>) : null}
+                <label><Icon name="clipboard list" />I want to...</label>
+                <Input required name="name" type="text" value={name} onChange={(e) => { setName(e.target.value) }} placeholder="learn to play piano"/>
+                {!name ? (<Label basic color="red" pointing="left">You must enter a habit!</Label>) : null}
               </Form.Field>
               <Form.Field>
-                <label>Description</label>
-                <TextArea required name="description" type="text" value={description} onChange={(e) => { setDescription(e.target.value) }} placeholder="I want to learn a new skill: playing the piano!" />
-                {!description ? (<Label basic color='red' pointing>You must enter a description!</Label>) : null}
+                <label>Description <Icon name="content" />{!description ? (<Label basic color="red">You must enter a description!</Label>) : null}</label>
+                <TextArea className={styles.description} required name="description" type="text" value={description} onChange={(e) => { setDescription(e.target.value) }} placeholder="I want to learn a new skill: playing the piano!" />
               </Form.Field>
               <Button type='submit' disabled={disableSubmit}>Submit</Button>
             </Form>
