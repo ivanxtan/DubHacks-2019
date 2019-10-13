d7382ffef6553b2ed4b92523141bfd7dd3da26b1
yehgen <yehgen@uw.edu>
2019-10-12 21:25:10 -0700

    add habitlist

--- src/components/presentational/HabitList/HabitList.js
+++ src/components/presentational/HabitList/HabitList.js
@@ -3,12 +3,22 @@ import React from "react";
 import HabitItem from "../HabitItem/HabitItem";
 
 function HabitList(props) {
-  let title = props.title;
-  let items = props.items;
-  return (
+    let title = props.title;
+    let items = props.items;
+
+    let itemsElements = items.map((item) => {
+        return (
+            <li>
+                <HabitItem values={item} />
+            </li>
+        );
+    });
+    return (
         <>
-          <h1>{title}</h1>
-          <HabitItem values={items[0]} />
+            <h1>{title}</h1>
+            <ul>
+                {itemsElements}
+            </ul>
         </>
     );
 }
